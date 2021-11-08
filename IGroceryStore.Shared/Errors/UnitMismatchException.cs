483696854456de13d8078db15a63516ab802e098
Adrian Franczak <44712992+Nairda015@users.noreply.github.com>
2021-11-09 00:06:46 +0100

    Add UnitMismatchException.cs

---
+++ IGroceryStore.Shared/Errors/UnitMismatchException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace IGroceryStore.Shared.Errors
+{
+    public class UnitMismatchException : Exception
+    {
+        public UnitMismatchException(string message) : base(message)
+        {
+        }
+    }
+}
